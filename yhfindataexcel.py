33e59c4144d34742e4017e6a0447418d906a55a8
WilliamPython1 <119360227+WilliamPython1@users.noreply.github.com>
2023-09-03 20:30:53 +0100

    Update yhfindataexcel.py

--- yhfindataexcel.py
+++ yhfindataexcel.py
@@ -5,7 +5,7 @@ stock_symbols = ['AAPL', 'MSFT', 'GOOGL']
 data = yf.download(stock_symbols, start='2023-01-01', end='2023-08-19')
 df = pd.DataFrame(data)
 
-excel_file_path = 'C:\\Users\\willi\\Downloads\\Book1.xlsx'
+excel_file_path = '(file_path)'
 df.to_excel(excel_file_path, index=True)
 
 print(f"Data saved to {excel_file_path}")
