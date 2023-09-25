55525f41f8496646e015c50f8b2beca8fb1601d3
WilliamPython1 <119360227+WilliamPython1@users.noreply.github.com>
2023-09-25 16:56:56 +0100

    With real date and creates document
    Real date is used and os is used for creating the document if it does not exist and editing it if it does.

--- yhfindataexcel.py
+++ yhfindataexcel.py
@@ -1,11 +1,31 @@
 import yfinance as yf
 import pandas as pd
+import datetime
+import os
+
+# os for creating document and replacing data if it already exists.
 
 stock_symbols = ['AAPL', 'MSFT', 'GOOGL']
-data = yf.download(stock_symbols, start='2023-01-01', end='2023-08-19')
+
+# Get the date one week ago and four days ago
+end_date = datetime.datetime.now().date() - datetime.timedelta(days=4)
+start_date = datetime.datetime.now().date() - datetime.timedelta(days=11)  # 7 days plus 4 more days
+
+data = yf.download(stock_symbols, start=start_date, end=end_date)
 df = pd.DataFrame(data)
 
-excel_file_path = '(file_path)'
-df.to_excel(excel_file_path, index=True)
+# Get the user's Documents directory and save the file there
+documents_dir = os.path.join(os.path.expanduser('~'), 'Documents')
+excel_file_path = os.path.join(documents_dir, 'Book1.xlsx')
+
+print(f"Saving data to: {excel_file_path}")
+
+# Close the file if it's open, won't work without.
+try:
+    df.to_excel(excel_file_path, index=True)
+except PermissionError:
+    print("Please close the Excel file and run the script again.")
 
 print(f"Data saved to {excel_file_path}")
+print(f"Start date: {start_date}")
+print(f"End date: {end_date}")
